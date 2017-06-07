d817d6dc62fa92faad53f767beaff92a15dfbe26
armenh <avarmenhg@gmail.com>
2017-06-07 15:42:18 -0400

    commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,17 +2,32 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
+import { AgGridModule } from 'ag-grid-angular/main';
 
 import { AppComponent } from './app.component';
+import { GridComponent } from './grid-component/grid-component.component';
+import { NameAndAgeRendererComponent } from './name-and-age-renderer/name-and-age-renderer.component';
+import { HeaderComponent } from './header/header.component';
+import { HeaderGroupComponent } from './header-group/header-group.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    GridComponent,
+    NameAndAgeRendererComponent,
+    HeaderComponent,
+    HeaderGroupComponent
   ],
   imports: [
     BrowserModule,
     FormsModule,
-    HttpModule
+    HttpModule,
+    AgGridModule.withComponents(
+      [
+        //NameAndAgeRendererComponent,
+        HeaderComponent,
+        HeaderGroupComponent
+      ]),
   ],
   providers: [],
   bootstrap: [AppComponent]
